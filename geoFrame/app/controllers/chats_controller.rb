79b2d5f517cbcaf56e41274b2eaf9fda97f8957a
robbiealixsantos <robbiealixsantos@gmail.com>
2016-12-12 15:49:26 -0800

    Add chats controller routes and rename chat name to title in migrations

--- geoFrame/app/controllers/chats_controller.rb
+++ geoFrame/app/controllers/chats_controller.rb
@@ -2,5 +2,26 @@ class ChatsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @chats = Chat.all
+  end
+
+  def new
+    @chat = Chat.new
+  end
+
+  def create
+    @chat_room = current_user.chat_rooms.build(chat_room_params)
+    if @chat_room.save
+      flash[:success] = 'Chat room added!'
+      redirect_to chat_rooms_path
+    else
+      render 'new'
+    end
+  end
+
+private
+
+  def chat_room_params
+    params.require(:chat_room).permit(:title)
   end
 end
