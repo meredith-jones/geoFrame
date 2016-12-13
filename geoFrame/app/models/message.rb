1f8a283a6f488e5afb839b96298b3d9bb6f172fb
robbiealixsantos <robbiealixsantos@gmail.com>
2016-12-12 16:33:44 -0800

    Add additional client side chat implementation - rooms.coffee

--- geoFrame/app/models/message.rb
+++ geoFrame/app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat
 
+  validates :body, presence: true, length: {minimum: 2, maximum: 1000}
+
   def timestamp
     created_at.strftime('%H:%M:%S %d %B %Y')
   end
